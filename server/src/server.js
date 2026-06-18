17d52c28c59b65d98383a75811e0153ac81d2b31
neelimagedela <neelimagedela3738@gmail.com>
2026-06-18 12:12:57 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -72,7 +72,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 /* =========================
    BODY PARSERS
